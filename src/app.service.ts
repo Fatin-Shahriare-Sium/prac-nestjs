13b63b91e26e082e4a393e622c0704d4e478b63f
fatin shahriare sium <sium1206@gmail.com>
2022-02-25 04:36:53 +0000

    alhamdulillah,learned the fundamentals of Nestjs by the grace of Allah

--- src/app.service.ts
+++ src/app.service.ts
@@ -18,7 +18,7 @@ export class AppService {
 
     console.log(x in Direction);
     console.log(Direction['down']);
-    return 'I am learning nestjs by the grace of Allah,alhamdulillah.And also i have fixed the hot reloading issues in my phone by the grace of Allah';
+    return 'I am learning nestjs by the grace of Allah,alhamdulillah.And also i have fixed the hot reloading issues in my phone by the grace of Allah.And,finally learned it easily by the grace of Allah';
 
   }
 }
